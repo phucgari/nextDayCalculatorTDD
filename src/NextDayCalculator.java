30e345fde76fbb82c516142f42442dd3197ec8b3
phucgari <phucgari@gmail.com>
2023-01-21 20:10:11 +0700

    fixxed extra

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -41,10 +41,7 @@ public class NextDayCalculator {
     boolean isLeapYear(int year){
         if(year%4==0){
             if(year%100==0){
-                if(year%400==0){
-                    return true;
-                }
-                return false;
+                return year % 400 == 0;
             }
             return true;
         }else{
